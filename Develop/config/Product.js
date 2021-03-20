dbcfd5422f5b451676a1290ec1d965f9bf0b002c
The Noise <leespaulding25@gmail.com>
2021-03-20 15:11:45 -0400

    added to product.js

--- Develop/config/Product.js
+++ Develop/config/Product.js
@@ -4,34 +4,49 @@ const { Model, DataTypes } = require('sequlize');
 const sequelize = require('../config/connection');
 
 //Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
     {
         id: {
             type: DataTypes.INTEGER,
-            primaryKey:true,
-            allowNull:false,
-            autoIncrement:true,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
         },
         product_name: {
             type: DataTypes.STRING,
-            allowNull:false,
+            allowNull: false,
         },
-        price:{
-            type:DataTypes.DECIMAL,
-            allowNull:false,
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
             validate: {
-                isDECIMAL:true
+                isDECIMAL: true
             }
         },
-            stock: {
-                type:DataTypes.INTEGER,
-                allowNull:false,
-                validate:{
-                    isDECIMAL:true
-                }
-            },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isDECIMAL: true
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references:{
+                model:'category',
+                key:'id'
+            }
         }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+
+    }
 )
