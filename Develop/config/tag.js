5ddc2c940573edc111d1d37ad2fd079dc7c81e2a
The Noise <leespaulding25@gmail.com>
2021-03-20 15:49:07 -0400

    added col to tag.js

---
+++ Develop/config/tag.js
@@ -0,0 +1,20 @@
+const{ Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+const { Tag } = require('./index.js');
+
+class Tag extends Model {}
+
+Tag.init(
+    {
+        id: {
+            type:DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull:false,
+            autoIncrement:true
+        },
+        tag_name: {
+            type:DataTypes.STRING
+        }
+    },
+)
